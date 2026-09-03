f14d865145513ade59c41b79a8a56b1b2be0fd16
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-03 09:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class Person {
 	private _age : number 
 	constructor(name: string, age: number){
 		this._name = name
-		this._age = agege
+		this._age = age
 	}
 	get name(): string{
 		return this._name
